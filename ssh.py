3e2149cd1811fd3865c0262d1139414aeb5aa823
Scott Duncombe <srduncombe@gmail.com>
2013-01-11 20:39:29 -0800

    rebuilt parminko based ssh class - with better error handling

---
+++ ssh.py
@@ -0,0 +1,39 @@
+import paramiko, base64
+
+class SSH:
+    base_dir = None
+    
+    def __init__(self,host,user,password,base_dir=None):
+        
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+        
+        self.client.connect(host, username=user, password=password)
+        
+        if base_dir is not None:
+            self.cd(base_dir)
+
+    def cd(self,dir):
+        self.base_dir = dir
+    
+    def c(self,command):
+        if self.base_dir is not None:
+            command = 'cd '+self.base_dir+';'+command
+            
+        stdin, stdout, stderr = self.client.exec_command(command)
+
+        error = stderr.readlines()
+
+        if len(error) > 0:
+            raise Exception(error)
+        else:
+            return stdout.readlines()
+
+    def p(self,command):
+        result = self.c(command)
+
+        for line in result:
+            print line.strip()
+        
+    def close(self):
+        ssh.close()
