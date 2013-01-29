031e355d24d3b20d99f60c1d77c815924fb214a3
Scott Duncombe <srduncombe@gmail.com>
2013-01-28 18:45:57 -0800

    explainer text for doit.py

--- doit.py
+++ doit.py
@@ -1,3 +1,16 @@
+# In this script I moved WordPress sites between two hosts - BlueHost and WebFaction - using SSH, mysqldump, and the WebFaction API
+# It demonstrates:
+#  > Using SSH and SCP to move files between sites
+
+## Note, you'll need to make sure keys are exchanged from the host you are moving to the host you're leaving (in this case WebFaction > Bluehost)
+## See for a how-to: http://www.howtogeek.com/66776/how-to-remotely-copy-files-over-ssh-without-entering-your-password/
+
+#  > Using WebFaction API to create sites
+
+#  > Using SSH to do a MySQL backup, import, and a command
+
+# See config/sites.txt for the layout of the site. This wont work 100% out of the box - you'll need to modify some of the routes in models/sites.py
+
 from models.sites import *
 from models.ssh import *
 from models.webfaction import *
