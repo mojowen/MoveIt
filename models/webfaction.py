fe9c8e3305134affb86f70030f36838c46b86c08
Scott Duncombe <srduncombe@gmail.com>
2014-01-19 18:41:34 -0800

    spacing

--- models/webfaction.py
+++ models/webfaction.py
@@ -50,59 +50,59 @@ Commands:
         result = result.split("'")[0]
         return result
 
-    
+
     def create_site(self,name,domain=None):
         name = name.replace('_','') # Subdomains can't have underscore
-        
+
         domains = [ name+"."+self.base_domain ]
-        
+
         # If we pass a domain - then create a domain
         if domain is not None:
             domains.append(domain)
             domains.append('www.'+domain)
             self.server.create_domain(self.session_id, domain,'www')
-        
+
         # Creating a subdomain
         self.server.create_domain(self.session_id, self.base_domain, name )
-        
+
         #Create the web site
         self.website = self.server.create_website( self.session_id, name, self.ip_address, False, domains, [name, '/'] )
-        
+
 
     def create_static(self,name,domain=None):
-        
+
         self.wordpress_app = self.server.create_app( self.session_id, name, 'static_php54',False)
         self.create_site(name,domain)
-        
+
     def c(self,command):
         directory = '';
-        
+
         if self.base_directory is not None:
             directory = 'cd %s;' % self.base_directory
-        
+
         return self.server.system( self.session_id, directory+command ).split("\n")
 
     def cd(self,directory):
         self.base_directory = directory
-    
-    
+
+
     def sql(self,command,username,password,db):
         sql = ['mysql -u',username,'--password='+password,'-D '+db,'-e',command]
         sql = " ".join(sql)
-    
+
     def c_wordpress(self,command):
         include = 'include_once("wp-load.php"); include_once("wp-admin/includes/admin.php"); '
-        
+
         command = include+command
-        
+
         return self.c("php -r '%s' " % command )
-    
+
     def get_latest_wordpress(self):
         apps = self.server.list_app_types( self.session_id )
         latest_version = 0
 
         for app in apps:
-            
+
             if app['name'].find('wordpress') != -1:
 
                 try:
@@ -121,11 +121,11 @@ Commands:
         self.server.create_user( self.session_id, name,'bash',['mojowen'] )
 
         self.server.change_user_password( self.session_id, name, password)
-        
+
         time.sleep(10) # Hang out for a sec till the password is set for the SSH account
 
         # Setting up the user account
-        from models.ssh import SSH 
+        from models.ssh import SSH
         user = SSH(self.base_domain, name, password)
         user.c('chmod 771 .')
         user.c('mkdir webapps')
@@ -149,26 +149,30 @@ Commands:
 
 
 
+    def set_default_wordpress_admin(self,email,password):
+        self.default_wordpress_admin = { "email": email, "password": password }
 
     def create_wordpress(self,name,domain=None,admin=False,new_user=False):
-        
+
         if self.wordpress_install is None: self.get_latest_wordpress()
-        
+
         self.wordpress_app = self.server.create_app( self.session_id, name, self.wordpress_install,True)
-                
+
         self.create_site(name,domain)
-                
+
         self.cd( '~/webapps/%s/' % name )
-        
+
         self.c_wordpress('update_option("siteurl","http://'+name+"."+self.base_domain+'"); update_option("home","http://'+name+"."+self.base_domain+'");' )
 
 
         # Do something about permalinks
         # Do something about visiability to search engines
-        
+
         # - Update admin email / password if requested
+        if not admin and self.default_wordpress_admin: admin = self.default_wordpress_admin
+
         if admin:
-            update_user = 'wp_update_user( array ("ID" => 1, "user_email" => "'+admin["email"]+'") ); wp_set_password("'+admin["password"]+'",1);'
+            update_user = 'wp_update_user( array ("user_login" => "srdmin", ID" => 1, "user_email" => "'+admin["email"]+'") ); wp_set_password("'+admin["password"]+'",1);'
             self.c_wordpress(update_user)
 
         # TO DO:
@@ -191,9 +195,9 @@ Commands:
             ['aspmx2.googlemail.com',20],
             ['aspmx3.googlemail.com',20]
         ]
-        
+
         for mx_record in mx_records:
-            self.server.create_dns_override( self.session_id, 
+            self.server.create_dns_override( self.session_id,
                 domain,
                 '', # An IP Address
                 '', # CNAME
@@ -202,11 +206,11 @@ Commands:
                 ''
             )
         dns_overrides = ['sites','calendar','docs','mail']
-        
+
         for override in dns_overrides:
             subdomain = override+'.'+domain
             self.server.create_domain(self.session_id, domain,  override)
-            self.server.create_dns_override( self.session_id, 
+            self.server.create_dns_override( self.session_id,
                 subdomain,
                 '',
                 'ghs.google.com', # CNAME
@@ -214,8 +218,8 @@ Commands:
                 '',
                 ''
             )
-     
-    # API for interfacing with WebFactions email: 
+
+    # API for interfacing with WebFactions email:
     # http://docs.webfaction.com/xmlrpc-api/apiref.html#email
     def create_forward(self,forwarding_address,forward_to):
         try:
@@ -225,7 +229,7 @@ Commands:
             error = err.faultString
             # error = error.split(":[u'")[1][:-2] Way to parse the string that's returned
             return error
-    
+
     def update_forward(self, forwarding_address,forward_to):
         try:
             new_email = self.server.update_email(self.session_id, forwarding_address, forward_to)
